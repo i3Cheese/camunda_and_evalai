ea6c126ee365b4cd7bba7a60c20f6d53907d6e2b
i3Cheese <i3Cheese@yandex.ru>
2024-04-21 14:56:59 +0300

    decision

--- camunda/src/main/kotlin/ru/i3cheese/camundakotlin/delegates/CreateChallengeHostTeam.kt
+++ camunda/src/main/kotlin/ru/i3cheese/camundakotlin/delegates/CreateChallengeHostTeam.kt
@@ -3,14 +3,10 @@ package ru.i3cheese.camundakotlin.delegates
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import okhttp3.OkHttpClient
-import okhttp3.MediaType
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.Request
-import okhttp3.RequestBody
-import okhttp3.RequestBody.Companion.toRequestBody
 import org.camunda.bpm.engine.delegate.DelegateExecution
 import org.camunda.bpm.engine.delegate.JavaDelegate
+import org.camunda.bpm.engine.variable.value.StringValue
+import org.camunda.bpm.engine.variable.value.TypedValue
 import org.springframework.stereotype.Component
 import ru.i3cheese.camundakotlin.evalai.EvalAISession
 
@@ -20,9 +16,11 @@ class CreateChallengeHostTeam: JavaDelegate {
     @Serializable
     private data class ChallengeHostTeamBody(val team_name: String, val team_url: String)
     override fun execute(execution: DelegateExecution) {
+        val teamName: String = execution.getVariableTyped<StringValue>("teamName", true).value;
+        println("Creating team $teamName")
         val response = EvalAISession.doPost("/api/hosts/challenge_host_team/",
             Json.encodeToString(
-                ChallengeHostTeamBody("test_team_name", "test_team_url")
+                ChallengeHostTeamBody(teamName, "test_team_url")
             )
         )
         println(response)
